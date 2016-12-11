d247cd674b74d2dc43271873908620d48aaecd45
nmjmb <moonjin@gmail.com>
2016-12-11 11:10:13 +0900

    first commit

---
+++ iossample1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iossample1
+//
+//  Created by tomatomb on 2016. 12. 11..
+//  Copyright © 2016년 nmj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
