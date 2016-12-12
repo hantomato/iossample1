baa313b0e100d0258eb882217927846d2145a998
nmj <moonjin@gmail.com>
2016-12-12 14:33:17 +0900

    git test

--- iossample1/AppDelegate.m
+++ iossample1/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"app launched.");
+    
     return YES;
 }
 
